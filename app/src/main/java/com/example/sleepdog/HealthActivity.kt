eff252b360de78be1f05f07f8d6a6743ed6505f1
minhee 318 <alsgml0867@naver.com>
2020-09-24 06:04:26 +0900

    UI 1차완성

--- app/src/main/java/com/example/sleepdog/HealthActivity.kt
+++ app/src/main/java/com/example/sleepdog/HealthActivity.kt
@@ -1,11 +1,16 @@
 package com.example.sleepdog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class HealthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_health)
+    }
+    fun onBackButtonClicked(view: View){
+        finish()
     }
 }
--- app/src/main/java/com/example/sleepdog/MainActivity.kt
+++ app/src/main/java/com/example/sleepdog/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.sleepdog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SettingsSlicesContract
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    var TAG: String = "로그"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "MainActivity-onCreate() called")
+    }
+
+    fun onSleepConfirmButtonClicked(view: View){
+        val intent = Intent(this,HealthActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onModifiedButtonClicked(view: View){
+        val intent2 = Intent(this,SettingActivity::class.java)
+        startActivity(intent2)
     }
 }
