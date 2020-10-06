71c0672f6db2de19c46ab53c5372a515a59d546b
minhee 318 <alsgml0867@naver.com>
2020-10-06 12:02:53 +0900

    login again

--- app/src/main/java/com/example/sleepdog/LoginActivity.kt
+++ app/src/main/java/com/example/sleepdog/LoginActivity.kt
@@ -1,18 +1,35 @@
 package com.example.sleepdog
 
+
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import com.kakao.auth.Session
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private var callback: SessionCallback = SessionCallback()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        Session.getCurrentSession().addCallback(callback);
+    }
+    @SuppressLint("MissingSuperCall")
+    override fun onDestroy() {
+        Session.getCurrentSession().removeCallback(callback);
     }
 
-    fun onIntoMainButtonClicked(view: View){
-        val intent4 = Intent(this,MainActivity::class.java)
-        startActivity(intent4)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            Log.i("Log", "session get current session")
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
+
+
+
 }
