f9cb5587f84c24470a575be514fb7bec0e7722df
minhee 318 <alsgml0867@naver.com>
2020-10-06 15:37:00 +0900

    logout

--- app/src/main/java/com/example/sleepdog/MainActivity.kt
+++ app/src/main/java/com/example/sleepdog/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.provider.SettingsSlicesContract
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.LogoutResponseCallback
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +19,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Log.d(TAG, "MainActivity-onCreate() called")
+
+        logout.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("로그아웃 하시겠습니까?")
+            builder.setPositiveButton("확인") { dialogInterface, i ->
+                UserManagement.getInstance().requestLogout(object : LogoutResponseCallback() {
+                    override fun onCompleteLogout() {
+                        val intent = Intent(applicationContext, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                })
+                dialogInterface.dismiss()
+            }
+            builder.setNegativeButton("취소") { dialogInterface, i ->
+                dialogInterface.dismiss()
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+
+        }
+
     }
+
+
 
     fun onSleepConfirmButtonClicked(view: View){
         val intent = Intent(this,HealthActivity::class.java)
